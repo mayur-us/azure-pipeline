a330017ed0ab2a7136f0334e31aa402adb393314
mayur-us <mayur.us@gmail.com>
2020-03-06 04:23:42 +0000

    changes in controller

--- basic-hello-world-rest-api/src/main/java/com/lending/rest/webservices/restfulwebservices/HelloWorldController.java
+++ basic-hello-world-rest-api/src/main/java/com/lending/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,14 +9,14 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World ";
+		return "Hello World . I am deploying a new version";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Azure V2");
+		return new HelloWorldBean("Azure V2. I am deploying a new version");
 	}
 
 	/// hello-world/path-variable/in28minutes
